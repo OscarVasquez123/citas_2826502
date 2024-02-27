3f6381d2988348533624ffff790168eb23c29476
unknown <Aprendiz@BOGDFPCGMFN4162.FormacionSena.RED>
2024-02-27 16:49:41 -0500

    Entidades y Relaciones Terminadas

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app , db)
 
 ##importar los modelos
-from .models import Medico
+from .models import Medico, Paciente, Consultorio, Cita
 
 ##ejutar el objeto
 if __name__ == '__main__':
